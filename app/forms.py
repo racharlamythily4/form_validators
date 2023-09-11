da6f222ced07e1dfbe688e0e15f2a6a518e7ad4c
racharlamythily <racharlamythily4@gmail.com>
2023-09-11 00:12:14 -0500

    first commit

--- app/forms.py
+++ app/forms.py
@@ -4,11 +4,11 @@ def check_for_m(value):
         raise forms.ValidationError('Started with m ')
     
 def check_for_len(value):
-    if len(value)<5:
+    if len(value)>5:
         raise forms.ValidationError('Length is Exceded')
 
 class StudentForms(forms.Form):
-    Sname=forms.CharField(max_length=100,validators=[check_for_m, check_for_len])
+    Sname=forms.CharField(max_length=100,validators=[check_for_len])
     Sage=forms.IntegerField()
     Sid=forms.IntegerField()
     Semail=forms.EmailField(validators=[check_for_m])
